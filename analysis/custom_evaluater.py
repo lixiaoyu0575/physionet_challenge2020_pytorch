3273fd8bf52c31715b2c32b7820fd59682d72300
Xiaoyu Li <lixiaoyu0575@qq.com>
2020-08-08 22:42:09 +0800

    analysing losses

--- analysis/custom_evaluater.py
+++ analysis/custom_evaluater.py
@@ -32,15 +32,15 @@ class Evaluater(BaseEvaluater):
 
                 output = self.model(data)
 
-                loss = self.criterion(output, target)
-                # self.test_metrics.update('loss', loss.item())
-                print(loss)
-                losses.append(loss.cpu().detach().numpy())
-
-                # output_logit = self.sigmoid(output)
-                # for met in self.metric_ftns:
-                #     self.test_metrics.update(met.__name__, met(self._to_np(output_logit), self._to_np(target)))
-            losses_array = np.concatenate(losses)
+                loss2save = self.criterion(output, target)
+                print()
+                losses.append(loss2save.cpu().detach().numpy())
+                loss = torch.mean(loss2save)
+                self.test_metrics.update('loss', loss.item())
+                output_logit = self.sigmoid(output)
+                for met in self.metric_ftns:
+                    self.test_metrics.update(met.__name__, met(self._to_np(output_logit), self._to_np(target)))
+            losses_array = np.concatenate(loss2save)
             np.save('./test_losses.npy', losses_array)
             print('save losses')
         result = self.test_metrics.result()
--- analysis/test.py
+++ analysis/test.py
@@ -1,11 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
-losses = np.load('test_losses.npy')
-for i in range(108):
-    losses_per_sample = np.sum(losses[:, i:i+1], axis=1)
-    # plt.hist(losses[0], bins=200, color='blue')
-    # plt.show()
-    plt.hist(losses_per_sample, bins=200, color='blue')
-    plt.show()
+
+from scipy.io import loadmat
+losses = np.load('losses.npy')
+indices = loadmat('../evaluation/scored_classes_indices.mat')['val']
+indices = indices.reshape([indices.shape[1],]).astype(bool)
+losses = losses[:, indices]
+# losses = losses.reshape((len(losses)*24))
+
+# #for samples
+# sample_losses = np.sum(losses, axis=1)
+# plt.hist(sample_losses, bins=200, color='blue')
+# plt.show()
+
+#for class
+class_loss = np.sum(losses.T, axis=1)
+plt.hist(class_loss, bins=200, color='blue')
+plt.show()
+
+# for i in range(108):
+#     losses_per_sample = np.sum(losses[:, i:i+1], axis=1)
+#     # plt.hist(losses[0], bins=200, color='blue')
+#     # plt.show()
+#     plt.hist(losses_per_sample, bins=200, color='blue')
+# #     plt.show()
+# plt.hist(losses, bins=200, color='blue')
+# plt.show()
 
 print('done')
