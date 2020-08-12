01383c8da7bb4926e3203de81374d356f097225f
lh20180514 <18629005576@163.com>
2020-08-12 12:24:45 +0800

    update dataloder

--- base/base_data_loader.py
+++ base/base_data_loader.py
@@ -198,3 +198,53 @@ class BaseDataLoader2(DataLoader):
             test_data_loader.n_samples = self.test_n_samples
             test_data_loader.idx = self.test_idx
             return test_data_loader
+
+class BaseDataLoader3(DataLoader):
+    """
+    Base class for all data loaders
+    """
+
+    def __init__(self, train_dataset, val_dataset, test_dataset,  batch_size, shuffle, num_workers,
+                 collate_fn=default_collate, pin_memory=True, normalization=False):
+        self.train_dataset = train_dataset
+        self.val_dataset = val_dataset
+        self.test_dataset = test_dataset
+        self.batch_size = batch_size
+        self.batch_idx = 0
+        self.shuffle = shuffle
+
+        self.init_kwargs = {
+            'dataset': self.train_dataset,
+            'batch_size': batch_size,
+            'shuffle': self.shuffle,
+            'collate_fn': collate_fn,
+            'num_workers': num_workers,
+            'pin_memory': pin_memory
+        }
+        super().__init__(**self.init_kwargs)
+
+        self.valid_data_loader_init_kwargs = {
+            'dataset': self.val_dataset,
+            'batch_size': batch_size,
+            'shuffle': self.shuffle,
+            'collate_fn': collate_fn,
+            'num_workers': num_workers,
+            'pin_memory': pin_memory
+        }
+
+        self.valid_data_loader = DataLoader(**self.valid_data_loader_init_kwargs)
+
+        self.test_data_loader_init_kwargs = {
+            'dataset': self.test_dataset,
+            'batch_size': batch_size,
+            'shuffle': False,
+            'collate_fn': collate_fn,
+            'num_workers': num_workers,
+            'pin_memory': pin_memory
+        }
+
+        self.test_data_loader = DataLoader(**self.test_data_loader_init_kwargs)
+
+        self.n_samples = len(self.train_dataset)
+        self.valid_data_loader.n_samples = len(self.val_dataset)
+        self.test_data_loader.n_samples = len(self.test_dataset)
--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -5,7 +5,7 @@ from scipy.io import loadmat
 import torch
 from torchvision import datasets, transforms
 from torch.utils.data import TensorDataset, Dataset
-from base import BaseDataLoader, BaseDataLoader2
+from base import BaseDataLoader, BaseDataLoader2, BaseDataLoader3
 from utils.dataset import ECGDataset
 from sklearn.preprocessing import MinMaxScaler
 from utils.dataset import load_label_files, load_labels, load_weights
@@ -402,3 +402,131 @@ class ChallengeDataLoader7(BaseDataLoader2):
 
         self.valid_data_loader.file_names = file_names
         self.test_data_loader.file_names = file_names
+
+# DataLoader
+class ChallengeDataLoader2(BaseDataLoader3):
+    """
+    challenge2020 data loading
+    """
+    def __init__(self, label_dir, data_dir, split_index, batch_size, shuffle=True, num_workers=2, training=True, training_size=None, augmentations=None):
+        start = time.time()
+        self.label_dir = label_dir
+        self.data_dir = data_dir
+        print('Loading data...')
+
+        weights_file = 'evaluation/weights.csv'
+        normal_class = '426783006'
+        equivalent_classes = [['713427006', '59118001'], ['284470004', '63593006'], ['427172004', '17338001']]
+
+        # Find the label files.
+        print('Finding label...')
+        label_files = load_label_files(label_dir)
+        time_record_1 = time.time()
+        print("Finding label cost {}s".format(time_record_1 - start))
+
+        # Load the labels and classes.
+        print('Loading labels...')
+        classes, labels_onehot, labels = load_labels(label_files, normal_class, equivalent_classes)
+        time_record_2 = time.time()
+        print("Loading label cost {}s".format(time_record_2 - time_record_1))
+
+        # Load the weights for the Challenge metric.
+        print('Loading weights...')
+        weights = load_weights(weights_file, classes)
+        time_record_3 = time.time()
+        print("Loading label cost {}s".format(time_record_3 - time_record_2))
+
+        # Classes that are scored with the Challenge metric.
+        indices = np.any(weights, axis=0)  # Find indices of classes in weight matrix.
+
+        split_idx = loadmat(split_index)
+        train_index, val_index, test_index = split_idx['train_index'], split_idx['val_index'], split_idx['test_index']
+        train_index = train_index.reshape((train_index.shape[1],))
+        if training_size is not None:
+            train_index = train_index[0:training_size]
+        val_index = val_index.reshape((val_index.shape[1],))
+        test_index = test_index.reshape((test_index.shape[1],))
+
+        num_files = len(label_files)
+        train_recordings = list()
+        train_labels_onehot = list()
+
+        val_recordings = list()
+        val_labels_onehot = list()
+
+        test_recordings = list()
+        test_labels_onehot = list()
+
+        file_names = list()
+
+        for i in range(num_files):
+            recording, header, name = load_challenge_data(label_files[i], data_dir)
+            recording[np.isnan(recording)] = 0
+
+            file_names.append(name)
+            if i in train_index:
+                for j in range(recording.shape[0]):
+                    train_recordings.append(recording[j])
+                    train_labels_onehot.append(labels_onehot[i])
+            elif i in val_index:
+                for j in range(recording.shape[0]):
+                    val_recordings.append(recording[j])
+                    val_labels_onehot.append(labels_onehot[i])
+            else:
+                for j in range(recording.shape[0]):
+                    test_recordings.append(recording[j])
+                    test_labels_onehot.append(labels_onehot[i])
+
+        time_record_4 = time.time()
+        print("Loading data cost {}s".format(time_record_4 - time_record_3))
+
+        print(np.isnan(train_recordings).any())
+        print(np.isnan(val_recordings).any())
+        print(np.isnan(test_recordings).any())
+
+        # Get number of samples for each category
+        self.indices = indices
+
+        train_recordings = np.array(train_recordings)
+        train_labels_onehot = np.array(train_labels_onehot)
+
+        val_recordings = np.array(val_recordings)
+        val_labels_onehot = np.array(val_labels_onehot)
+
+        test_recordings = np.array(test_recordings)
+        test_labels_onehot = np.array(test_labels_onehot)
+
+        X_train = torch.from_numpy(train_recordings).float()
+        Y_train = torch.from_numpy(train_labels_onehot).float()
+
+        X_val = torch.from_numpy(val_recordings).float()
+        Y_val = torch.from_numpy(val_labels_onehot).float()
+
+        X_test = torch.from_numpy(test_recordings).float()
+        Y_test = torch.from_numpy(test_labels_onehot).float()
+
+        #############################################################
+        if augmentations:
+            transformers = list()
+
+            for key, value in augmentations.items():
+                module_args = dict(value['args'])
+                transformers.append(getattr(module_transformers, key)(**module_args))
+
+            train_transform = transforms.Compose(transformers)
+            self.train_dataset = CustomTensorDataset(X_train, Y_train, transform=train_transform)
+        else:
+            self.train_dataset = TensorDataset(X_train, Y_train)
+        #############################################################
+
+        self.val_dataset = TensorDataset(X_val, Y_val)
+        self.test_dataset = TensorDataset(X_test, Y_test)
+
+        end = time.time()
+        print('time to get and process data: {}'.format(end - start))
+        super().__init__(self.train_dataset, self.val_dataset, self.test_dataset, batch_size, shuffle, num_workers)
+
+        self.valid_data_loader.file_names = file_names
+        self.valid_data_loader.idx = val_index
+        self.test_data_loader.file_names = file_names
+        self.test_data_loader.idx = test_index
